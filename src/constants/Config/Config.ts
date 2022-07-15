cf7a5a901f589c87edf44e8d6a1e6cbce1c1d137
Stanislau Khamiankou <stanislau.khamiankou@leverx.com>
2022-07-15 11:10:25 +0300

    fix chart

--- src/constants/Config/Config.ts
+++ src/constants/Config/Config.ts
@@ -1,25 +1,25 @@
 import { dataProductСhart as data } from "src/constants"
 
 export const configProductСhart = {
-    appendPadding: 10,
-    data,
-    angleField: 'value',
-    colorField: 'type',
-    radius: 0.8,
-    label: {
-      type: 'outer',
+  appendPadding: 10,
+  data,
+  angleField: 'value',
+  colorField: 'type',
+  radius: 0.8,
+  label: {
+    type: 'outer',
+  },
+  interactions: [
+    {
+      type: 'element-active',
     },
-    interactions: [
-      {
-        type: 'element-active',
-      },
-    ], 
-} as const
+  ], 
+}
 
 export const configPercentageChart = {
-    height: 200,
-    width: 200,
-    autoFit: false,
-    percent: 0.7,
-    color: ['#5B8FF9', '#E8EDF3'],
-} as const
+  height: 200,
+  width: 200,
+  autoFit: false,
+  percent: 0.7,
+  color: ['#5B8FF9', '#E8EDF3'],
+}
